12226e26a657c78a6c2c7702a47de9696f0fd50c
Francis Langlois <francis.langlois@gmail.com>
2015-08-06 17:10:28 -0400

    Display info in the sidebar based on results displayed on map

--- gulpfile.js
+++ gulpfile.js
@@ -9,20 +9,20 @@ gulp.task('styles', function() {
 	//Return transformed file
 	//We use the gulp.src method to tell gulp what files to look at.
 	//This is a string that is a path that is relative to our gulpfile.js
-	return gulp.src ('styles/*.scss')
-		//We use the .pipe method to keep pushing the data along. gulp.src
-		//will return our .scss and .pipe will push it into another function
-		//in this case we push it into sass
-		.pipe(sass().on('error', sass.logError))
-		//The sass function, returns as the converted scss
-		//we use concat to create a file for it to go into
-		.pipe(concat('style.css'))
-		//Will return said file, and we use gulp.dest to tell gulp where to put it
-		.pipe(gulp.dest('styles/'));
+	return gulp.src ('css/*.scss')
+	//We use the .pipe method to keep pushing the data along. gulp.src
+	//will return our .scss and .pipe will push it into another function
+	//in this case we push it into sass
+	.pipe(sass().on('error', sass.logError))
+	//The sass function, returns as the converted scss
+	//we use concat to create a file for it to go into
+	.pipe(concat('style.css'))
+	//Will return said file, and we use gulp.dest to tell gulp where to put it
+	.pipe(gulp.dest('css/'));
 });
 
 gulp.task('watch', function() {
-	gulp.watch('styles/*.scss', ['styles']);	
+	gulp.watch('styles/*.scss', ['styles']);
 	gulp.watch('js/*.js', ['jshint']);
 });
 
